f4691c20647eaadaca32f69a2d351bb114f2e211
Elijah <eliyang256@outlook.com>
2026-08-15 13:37:47 +0300

    email management

--- accounts/views/__init__.py
+++ accounts/views/__init__.py
@@ -1,4 +1,4 @@
-
+from allauth.account.models import EmailAddress
 from django.shortcuts import render
 from django.views import View
 
@@ -10,4 +10,13 @@ accounts_router = Registrar(prefix="accounts", namespace="accounts")
 @accounts_router.route("profile", name="profile")
 class ProfileView(View):
     def get(self, request):
-        return render(request, "accounts/profile.html", {"user": request.user})
+        user = request.user
+        email_addresses = EmailAddress.objects.filter(
+            user=user,
+            email=user.email,
+        )
+        context = {
+            "user": user,
+            "email_addresses": email_addresses,
+        }
+        return render(request, "accounts/profile.html", context)
